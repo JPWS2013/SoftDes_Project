bc7e0148c6eafdfbf23464a014752ec96aedfe64
Zoe Fiddler <zoe.fiddler@students.olin.edu>
2013-12-15 22:24:53 -0500

    hi justin

--- Dash.py
+++ Dash.py
@@ -5,6 +5,7 @@ import random
 import math
 import modelclassdef as model
 import serial
+from PIL import Image, ImageTk
 
 class View:
     def __init__ (self,master,model):
@@ -12,33 +13,27 @@ class View:
         self.master=master
         # model.view=self
 
-        self.pot_value=StringVar()
-        self.pot_value.set('not workin')
-
-        self.accel_value=StringVar()
-        self.accel_value.set('Fuckity fuck fuck')
-
         self.now=StringVar()
         self.now.set('fuckin fuck')
 
-        self.steering=Label(master,textvar=self.pot_value, width=50, padx=5, pady=5, relief=RIDGE,font=('Helvetica',16))
-        self.steering.grid(row=0)
-
-        self.accel=Label(master,textvar=self.accel_value, width=50, padx=5, pady=5, relief=RIDGE, font=('Helvetica',16))
-        self.accel.grid(row=1)
-
         self.gaspedal=Canvas(master,width=250,height=250)
-        self.gaspedal.grid(row=0,column=1, rowspan=4)
+        self.gaspedal.grid(row=0,column=1)
         self.brakepedal=Canvas(master,width=250,height=250)
-        self.brakepedal.grid(row=0,column=2, rowspan=4)
+        self.brakepedal.grid(row=0,column=2)
 
         self.timer=Label(master,textvar=self.now, padx=5,pady=5,font=('Helvetica,16'))
-        self.timer.grid(row=2)
+        self.timer.grid(row=2, column=0)
 
         self.quit=Button(master,text='QUIT', command=master.quit, font=('Helvetica,12'))
         self.quit.grid(row=0, column=3)
 
-        self.speed=Label
+        self.speedo=Canvas(master, width=500, height=250)
+        self.speedo.grid(row=0,column=0)
+        image = Image.open("speedo.png")
+        photo = ImageTk.PhotoImage(image)
+        self.speedo.image=photo
+        self.speedo.create_image(300,125,image=photo)
+
 
         ser=serial.Serial('/dev/ttyACM0', 9600) #Defines the serial port to use
 
@@ -47,7 +42,7 @@ class View:
 
         if ser.isOpen():
             print "Database ready to receive data" 
-
+        ser=[]
         self.update(ser)
         master.mainloop()
         
@@ -76,31 +71,28 @@ class View:
         if command=='3':
             self.model.store_data_accel(datalist[0], datalist[1], datalist[2])
 
-
-        self.display_pot(random.random())
-        self.display_accel((random.random(),random.random(),random.random()))
         self.display_gaspedal()
-        self.display_brakepedal(random.random())
+        self.display_brakepedal()
         self.display_time(datetime.datetime.today())
+        self.display_speed()
         self.master.after(100,self.update, ser)
 
-
-    def display_pot(self, data):
-        self.pot_value.set('potentiometer: %g'%(data))
-
-
-    def display_accel(self,datatuple):
-        x=datatuple[0]
-        y=datatuple[1]
-        z=datatuple[2]
-        self.accel_value.set('X: %g \t Y: %g \t Z: %g' %(x,y,z))
+    def display_speed(self):
+        data=random.randrange(40)
+        theta=data*math.pi/40
+        length=100
+        base=math.cos(theta)*length
+        height=length*math.sin(theta)
+        self.speedo.delete(ALL)
+        self.speedo.create_image(300,125,image=self.speedo.image)
+        self.speedo.create_line(300,200,300-base,200-height, width=10, fill='red')
 
 
     def display_gaspedal(self):
         senseid='Potentiometer0'
         # print self.model.sensedict[senseid].keys()
         try:
-            data=self.model.lastpotreading#self.model.sensedict[senseid][self.model.sensedict[senseid].keys()[-1]]
+            data=self.model.sensedict[senseid][self.model.sensedict[senseid].keys()[-1]]
             self.gaspedal.delete(ALL)
             self.gaspedal.create_rectangle(0,0,250,250,fill='blue')
             length=250
@@ -121,15 +113,23 @@ class View:
             self.gaspedal.create_text(200,75,text='Gas Pedal', font='Helvetica,12')
 
 
-    def display_brakepedal(self,data):
-        self.brakepedal.delete(ALL)
-        self.brakepedal.create_rectangle(0,0,250,250,fill='red')
-        length=250
-        pot=data
-        height=length*math.sin(math.pi*pot/4+math.pi/6)
-        base=length*math.cos(math.pi*pot/4+math.pi/6)
-        self.brakepedal.create_line(0,0,base,height, width=20)
-        self.brakepedal.create_text(200,75,text='Brake Pedal', font='Helvetica,12')
+    def display_brakepedal(self):
+        try:
+            data=self.model.lastpotreading
+            self.brakepedal.delete(ALL)
+            self.brakepedal.create_rectangle(0,0,250,250,fill='red')
+            length=250
+            pot=data
+            height=length*math.sin(math.pi*pot/4+math.pi/6)
+            base=length*math.cos(math.pi*pot/4+math.pi/6)
+            self.brakepedal.create_line(0,0,base,height, width=20)
+            self.brakepedal.create_text(200,75,text='Brake Pedal', font='Helvetica,12')
+        except (KeyError,IndexError):
+            self.brakepedal.delete(ALL)
+            self.brakepedal.create_rectangle(0,0,250,250,fill='red')
+            self.brakepedal.create_line(0,0,0,250, width=20)
+            self.brakepedal.create_text(200,75,text='Brake Pedal', font='Helvetica,12')
+
 
 
     def display_time(self,datetime):
